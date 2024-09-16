1b81f90859e869013d92a1f30fba832291dcecbd
Sertan Akpinar <sert@MBP-von-Sertan.fritz.box>
2024-09-16 15:40:24 +0200

    final commit chart is added

--- basic/CodingChallenge.py
+++ basic/CodingChallenge.py
@@ -5,7 +5,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-load_dotenv('key.env')
+load_dotenv('../key.env')
 API_KEY = os.getenv('API_KEY')
 LOCATION = 'Berlin'
 URL = f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{LOCATION}/next14days?unitGroup=metric&key={API_KEY}&contentType=json'
@@ -32,7 +32,6 @@ for day in forecast:
     print(f"Windgeschwindigkeit: {wind_speed} km/h")
     print("------------------------------")
 
-     # E-Mail-Inhalt erstellen
     email_body += f"Datum: {date}\n"
     email_body += f"Minimale Temperatur: {min_temp}°C\n"
     email_body += f"Maximale Temperatur: {max_temp}°C\n"
@@ -40,7 +39,6 @@ for day in forecast:
     email_body += f"Windgeschwindigkeit: {wind_speed} km/h\n"
     email_body += "------------------------------\n"
 
-    # Erstellung des HTML-Inhalts
 html_content = f"""
 <html>
 <body>
@@ -48,22 +46,19 @@ html_content = f"""
 <ul>
 """
 
-# Wetterdaten im HTML-Format speichern
 for day in forecast:
     html_content += f"<li>{day['datetime']}: Min Temp: {day['tempmin']}°C, Max Temp: {day['tempmax']}°C, Regenwahrscheinlichkeit: {day['precipprob']}%, Windgeschwindigkeit: {day['windspeed']} km/h</li>"
 
 html_content += "</ul></body></html>"
 
-# Speichern der Vorhersage in einer HTML-Datei
 with open("wettervorhersage.html", "w") as file:
     file.write(html_content)
 
 print("\nDie Wettervorhersage wurde in der Datei 'wettervorhersage.html' gespeichert.")
 
-# Funktion zum Senden der E-Mail
 def send_email(subject, body, to_email):
-    from_email = os.getenv('E_MAIL')  # E-Mail-Adresse des Absenders
-    from_password = os.getenv('CODE')  # App-spezifisches Passwort für Gmail
+    from_email = os.getenv('E_MAIL')
+    from_password = os.getenv('CODE')
     msg = MIMEMultipart()
     msg['From'] = from_email
     msg['To'] = to_email
@@ -71,7 +66,6 @@ def send_email(subject, body, to_email):
 
     msg.attach(MIMEText(body, 'plain'))
 
-    # SMTP-Verbindung herstellen
     server = smtplib.SMTP('smtp.bht-berlin.de', 587)
     server.starttls()
     server.login(from_email, from_password)
@@ -79,7 +73,6 @@ def send_email(subject, body, to_email):
     server.sendmail(from_email, to_email, text)
     server.quit()
 
-# E-Mail mit der Wettervorhersage versenden
 to_email = os.getenv('E_MAIL')
 send_email(f"14-Tage-Wettervorhersage für {LOCATION}", email_body, to_email)
 
